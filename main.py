587454250ea065f281847f5fc10a8a64583604f9
Alexander-Degtyar <oleksandr.dehtiar@nure.ua>
2018-06-03 19:49:47 +0300

    Updated game skeleton

--- main.py
+++ main.py
@@ -2,16 +2,22 @@ import pygame
 
 BLACK = (0, 0, 0)
 
+WIDTH = 800
+HEIGHT = 600
+WINDOW_SIZE = (WIDTH, HEIGHT)
+FPS = 60
+
 pygame.init()
-pygame.mixer.init()
-screen = pygame.display.set_mode((800, 600))
-screen.fill(BLACK)
+screen = pygame.display.set_mode(WINDOW_SIZE)
+
 pygame.display.set_caption("GeekSchool Platformer")
 clock = pygame.time.Clock()
 
+all_sprites = pygame.sprite.Group()
+
 running = True
 while running:
-	clock.tick(60)
+	clock.tick(FPS)
 	
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
@@ -20,6 +26,10 @@ while running:
 			if event.key == pygame.K_ESCAPE:
 				running = False
 	
+	all_sprites.update()
+	
+	screen.fill(BLACK)
+	all_sprites.draw(screen)
 	pygame.display.flip()
 
 pygame.quit()
